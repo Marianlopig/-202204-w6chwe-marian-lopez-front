b9a07773872d70f8ac3aa92b3736837dd69fc4b0
Marian <marianlopig@gmail.com>
2022-05-14 18:26:10 +0200

    created the list from thunk get api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import RobotsList from "./components/RobotsList/RobotsList";
+import { loadRobotsThunk } from "./redux/thunks/robotsThunk";
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(loadRobotsThunk());
+  }, [dispatch]);
+
   return (
     <div className="App">
       <RobotsList />
