685ac77a9f8ed5dc93c1c72fd39f0f8314afb2cb
Marian <marianlopig@gmail.com>
2022-05-15 17:04:12 +0200

    tested deletereducer

--- src/redux/features/robotsSlice.test.js
+++ src/redux/features/robotsSlice.test.js
@@ -1,4 +1,7 @@
-import robotsReducer, { loadRobotsActionCreator } from "./robotsSlice";
+import robotsReducer, {
+  loadRobotsActionCreator,
+  deleteRobotActionCreator,
+} from "./robotsSlice";
 
 describe("Given a robotsSlice with an loadRobots reducer", () => {
   describe("When we pass a list of robots", () => {
@@ -10,6 +13,21 @@ describe("Given a robotsSlice with an loadRobots reducer", () => {
       const newState = robotsReducer(initialValue, action);
 
       expect(newState).toEqual(receivedValue);
+    });
+  });
+});
+
+describe("Given a robotsSlice with an deleteRobots reducer", () => {
+  describe("When we pass a robots id to delete", () => {
+    test("Then it should return the new list without the deleted robot", () => {
+      const initialValue = [{ _id: "1" }, { _id: "2" }, { _id: "3" }];
+      const idToDelete = "3";
+      const expectedRobotList = [{ _id: "1" }, { _id: "2" }];
+
+      const action = deleteRobotActionCreator(idToDelete);
+      const newState = robotsReducer(initialValue, action);
+
+      expect(newState).toEqual(expectedRobotList);
     });
   });
 });
