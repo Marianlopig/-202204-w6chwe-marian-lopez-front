46d9fef4ce2ceb1f75617836a7a3fa8c935a6ae7
Marian <marianlopig@gmail.com>
2022-05-16 01:04:51 +0200

    navigation added with css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Navigate, Route, Routes } from "react-router";
 import Form from "./components/Form/Form";
+import Navigation from "./components/Navigation/Navigation";
 import RobotsList from "./components/RobotsList/RobotsList";
 import { loadRobotsThunk } from "./redux/thunks/robotsThunk";
 
@@ -13,6 +14,7 @@ function App() {
 
   return (
     <div className="App">
+      <Navigation />
       <Routes>
         <Route path="/" element={<Navigate to="/home" />} />
         <Route path="/home" element={<RobotsList />} />
