a1f3ab0713feaee6c97154054d8274afd31c6d59
Marian <marianlopig@gmail.com>
2022-05-15 16:29:39 +0200

    fixes css in production

--- src/components/Robot/RobotStyles.js
+++ src/components/Robot/RobotStyles.js
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 
 export const RobotStyles = styled.div`
-  @import url("https://fonts.googleapis.com/css2?family=Righteous&display=swap");
   display: flex;
   .card {
     position: relative;
