7c379bef04a0a57fd5c7260fe61fe6a1ad6329ea
Marian <marianlopig@gmail.com>
2022-05-17 22:28:49 +0200

    adding login path

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import robotsReducer from "../redux/features/robotsSlice/robotsSlice";
+import usersReducer from "../redux/features/usersSlice/usersSlice";
 
 const store = configureStore({
-  reducer: { robots: robotsReducer },
+  reducer: { robots: robotsReducer, users: usersReducer },
 });
 
 export default store;
